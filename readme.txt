01f642ad388ea73a3f557fa6d6b8835e16fa8fd4
Conrad Rockenhaus <conrad@rockenhaus.com>
2023-08-06 01:12:09 -0400

    Initial Commit
    Version 1.0 Initial commit

---
+++ readme.txt
@@ -0,0 +1,16 @@
+=== Use jQuery CDN ===
+Contributors: conradrock
+plugin url: https://cannabytes.net/use-jquery-cdn
+Tags: CDN, CloudFlare, LightHouse, jquery, js, jquery-migrate, optimization, speed, WordPress Performance
+Donate link: conrad@cannabytes.net
+Requires at least: 6.2
+Tested up to: 6.2
+Stable tag: 1.0
+
+* An extremely light plugin to load jquery and jquery migrate scripts from CloudFlare CDNjs, which is extremely fast and is more than likely cached already by the enduser.
+* It accelerates the loading of at least 2 seconds.
+* It is written in 10 lines of code and weighs about 0.005 MB
+* It automatically detects the version of the script used by your theme and makes a request to it.
+
+== Changelog ==
+1.0 Initial Release
---
+++ use-jquery-cdnjs.php
@@ -0,0 +1,22 @@
+<?php
+ /*
+Plugin Name: Use jQuery CDNjs
+Plugin URI: https://cannabytes.net/use-jquery-cdnjs
+Donate link: conrad@cannabytes.net
+Description: Change your WordPress Installation to use CloudFlare CDNjs to serve jQuery
+Author: Conrad Rockenhaus (conradrock)
+Author URI: https://cannabytes.net
+Version: 1.0
+*/
+function modify_jquery() {global $wp_scripts;
+	if (!is_admin()) {
+		$jquery_ver = $wp_scripts->registered['jquery']->ver;
+		$jquery_migrate_ver = $wp_scripts->registered['jquery-migrate']->ver;
+		wp_deregister_script('jquery');
+		wp_deregister_script('jquery-migrate');
+		wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/$jquery_ver/jquery.min.js', false, null,true);
+		wp_enqueue_script('jquery-migrate', 'https://cdnjs.cloudflare.com/ajax/libs/$jquery_migrate_ver/jquery-migrate.min.js', false, null,true);
+		}
+}
+add_action('wp_enqueue_scripts', 'modify_jquery',9999);
+?>
