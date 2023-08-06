684f3df221a4241827b6e8b9c2e3222d8cabcd6a
Conrad Rockenhaus <conrad@rockenhaus.com>
2023-08-06 01:25:10 -0400

    Update use-jquery-cdnjs.php
    Fixed formatting errors for variables.

--- use-jquery-cdnjs.php
+++ use-jquery-cdnjs.php
@@ -14,8 +14,8 @@ function modify_jquery() {global $wp_scripts;
 		$jquery_migrate_ver = $wp_scripts->registered['jquery-migrate']->ver;
 		wp_deregister_script('jquery');
 		wp_deregister_script('jquery-migrate');
-		wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/$jquery_ver/jquery.min.js', false, null,true);
-		wp_enqueue_script('jquery-migrate', 'https://cdnjs.cloudflare.com/ajax/libs/$jquery_migrate_ver/jquery-migrate.min.js', false, null,true);
+		wp_enqueue_script('jquery', 'https://cdnjs.cloudflare.com/ajax/libs/jquery/'.$jquery_ver.'/jquery.min.js', false, null,true);
+		wp_enqueue_script('jquery-migrate', 'https://cdnjs.cloudflare.com/ajax/libs/'.$jquery_migrate_ver.'/jquery-migrate.min.js', false, null,true);
 		}
 }
 add_action('wp_enqueue_scripts', 'modify_jquery',9999);
